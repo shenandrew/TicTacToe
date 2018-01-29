39059aa2011e14fd6453a84f53a001e771b31a74
shenandrew <19shen_andrew@dublinstudents.net>
2018-01-29 10:37:08 -0500

    Put out the parts each person is doing

--- TickTackToe/src/tickTackToePackage/Board.java
+++ TickTackToe/src/tickTackToePackage/Board.java
@@ -10,4 +10,31 @@ public class Board {
 	public void setSpace(int id, int row, int col) {
 		gameBoard[row][col] = id;
 	}
+	
+	
+	//Returns the value of the specific space
+	//Srujan
+	public int getSpace(int row, int col) {
+		return 0;
+	}
+	
+	
+	//Checks if there is a winning condition on the board (3 in a row)
+	//Saket
+	public boolean checkCondition() {
+		return false;
+	}
+	
+	//Checks if there are empty spaces on the board left
+	//Andrew
+	public boolean checkAvailability() {
+		return false;
+	}
+	
+	//Prints out the board in X's and O's
+	//Maggie
+	public void getBoard() {
+		
+	}
+	
 }
