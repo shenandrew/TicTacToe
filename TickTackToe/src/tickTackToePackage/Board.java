1c0971e613aab2f1515b29b8ca2e6985c713f7ab
shenandrew <19shen_andrew@dublinstudents.net>
2018-01-30 17:23:11 -0500

    Finished checkAvailability()

--- TickTackToe/src/tickTackToePackage/Board.java
+++ TickTackToe/src/tickTackToePackage/Board.java
@@ -28,7 +28,15 @@ public class Board {
 	//Checks if there are empty spaces on the board left
 	//Andrew
 	public boolean checkAvailability() {
-		return false;
+		boolean available = false;
+		for(int i = 0; i < gameBoard.length; i++) {
+			for(int j = 0; j < gameBoard[0].length; j++) {
+				if(gameBoard[i][j] == 0) {
+					available = true;
+				}
+			}
+		}
+		return available;
 	}
 	
 	//Prints out the board in X's and O's
