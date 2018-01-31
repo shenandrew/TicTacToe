d9f9f48d0fa2a386b824076977ebfd87da29d05c
Chess Girl <Chess Girl@DESKTOP-34DIK1R>
2018-01-31 17:24:51 -0500

    ddads

--- TickTackToe/src/tickTackToePackage/GameDriver.java
+++ TickTackToe/src/tickTackToePackage/GameDriver.java
@@ -7,25 +7,26 @@ public class GameDriver {
 		Player player1 = new Player("Bob", 1);
 		Player player2 = new Player("Jack", 2);
 		Board board = new Board();
-				while(board.checkCondition())
-				{
-					if(board.checkAvailability())
-					{
-						player1.setMarker();
-						player1.checkwinner();
-					}
-					if(!board.checkAvailability())
-					{
-						player2.setMarker();
-						player2.checkwinner();
-					}
-					
-				}
-
+		while(board.checkCondition())
+		{
+			if(board.checkAvailability())
+			{
+				player1.setMarker();
+				player1.checkwinner();
 			}
+			if(!board.checkAvailability())
+			{
+				player2.setMarker();
+				player2.checkwinner();
+			}
+		board.getBoard();			
+				
+			
 
-		}
+	}
+
+		
 
 }
 
-}
+
