c8d6da1d366943449a3f66d6a7a4cc4e836afff3
shenandrew <19shen_andrew@dublinstudents.net>
2018-01-30 22:32:11 -0500

    finished checkCondition

--- TickTackToe/src/tickTackToePackage/Board.java
+++ TickTackToe/src/tickTackToePackage/Board.java
@@ -20,23 +20,34 @@ public class Board {
 	
 	
 	//Checks if there is a winning condition on the board (3 in a row)
-	//Saket
+	//Andrew
 	public boolean checkCondition() {
+		//Check each row
+		for(int i = 0; i < 3; i++) {
+			if(gameBoard[i][0] == gameBoard[i][1] && gameBoard[i][0] == gameBoard[i][2] && gameBoard[i][0] != 0) {
+				return true;
+			}
+		}
+		//Check each column
+		for(int j = 0; j < 3; j++) {
+			if(gameBoard[0][j] == gameBoard[1][j] && gameBoard[0][j] == gameBoard[2][j] && gameBoard[0][j] != 0) {
+				return true;
+			}
+		}
+		//Check diagonals
+		if(gameBoard[0][0] == gameBoard[1][1] && gameBoard[0][0] == gameBoard[2][2] && gameBoard[0][0] != 0) {
+			return true;
+		}
+		if(gameBoard[0][2] == gameBoard[1][1] && gameBoard[0][2] == gameBoard[2][0] && gameBoard[1][1] != 0) {
+			return true;
+		}
 		return false;
 	}
 	
 	//Checks if there are empty spaces on the board left
-	//Andrew
+	//Saket
 	public boolean checkAvailability() {
-		boolean available = false;
-		for(int i = 0; i < gameBoard.length; i++) {
-			for(int j = 0; j < gameBoard[0].length; j++) {
-				if(gameBoard[i][j] == 0) {
-					available = true;
-				}
-			}
-		}
-		return available;
+		return false;
 	}
 	
 	//Prints out the board in X's and O's
